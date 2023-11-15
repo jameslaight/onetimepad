e84ac972b8aaf93a744fa53738648128b4090d85
= <=>
2023-11-15 17:20:46 +0000

    add programs list to Fight

--- src/main/Fight.java
+++ src/main/Fight.java
@@ -1,16 +1,23 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Fight {
 
 	private final int level;
 	private final Clip clip;
 	private final Gate gate;
 
+	private final List<Program> programs = new ArrayList<>();
+
 	public Fight(int level) {
 		this.level = level;
 
 		clip = new Clip();
 		gate = new Gate(level - 1);
+
+		programs.add(new X(this));
 	}
 
 	public int getLevel() {
