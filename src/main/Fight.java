ddcf8855ca0a8fdb374d94a2acad770a68b1d42f
= <=>
2023-11-15 17:20:46 +0000

    added Fight.java

---
+++ src/main/Fight.java
@@ -0,0 +1,24 @@
+package main;
+
+public class Fight {
+
+	private final int level;
+	private final Clip clip;
+	private final Gate gate;
+
+	public Fight(int level) {
+		this.level = level;
+
+		clip = new Clip();
+		gate = new Gate(level - 1);
+	}
+
+	public Clip getClip() {
+		return clip;
+	}
+
+	public Gate getGate() {
+		return gate;
+	}
+
+}
