fe3c7de36a2b92a0e72f8f6a572de6676d382705
= <=>
2023-11-15 17:20:46 +0000

    added program X.java

---
+++ src/main/X.java
@@ -0,0 +1,14 @@
+package main;
+
+public class X extends Program { //needed to be short for a multiuse program, no inspiration taken from social media sites run by megalomaniacs
+
+	public X(String name) {
+		super(name);
+	}
+
+	@Override
+	public boolean execute(String[] args) {
+		return false;
+	}
+
+}
