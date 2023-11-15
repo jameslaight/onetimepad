5e58bec0ac2fb66a02cbbb1ec46f83eb7140c6e6
= <=>
2023-11-15 17:20:47 +0000

    set up upgrade code

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,11 @@
 package main;
 
 import program.Program;
+import upgrade.Upgrade;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -14,8 +17,12 @@ public class Main {
 	public void run() {
 		Scanner scanner = new Scanner(System.in);
 
+		List<Upgrade> upgrades = new ArrayList<>(); //all upgrades collected by the user
+
 		for (int level = 1; level <= 10; level++) {
 			Fight fight = new Fight(level);
+
+			upgrades.forEach(u -> u.apply(fight)); //apply upgrades to fight
 
 			while (fight.getGate().isAlive()) {
 				System.out.println("\t" + fight.getGate().getDisplay());
--- src/upgrade/Upgrade.java
+++ src/upgrade/Upgrade.java
@@ -1,5 +1,9 @@
 package upgrade;
 
+import main.Fight;
+
 public abstract class Upgrade {
+
+	public abstract void apply(Fight fight);
 
 }
